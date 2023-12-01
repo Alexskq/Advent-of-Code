036013e44ce7787c2ebe91bc9c2a78d83c7a1a07
Zoonekynd Alexandre <alexzoonekynd@gmail.com>
2023-12-01 21:19:46 +0100

    add steps

---
+++ Day_1/app.js
@@ -0,0 +1,7 @@
+// 1 - mettre le text dans une variable de type string
+// 2 - split après chaque espace : \n
+// 3 - push chaque élement dans un tableau
+// 4 - sur chaque élément itérer sur chaque caractère et récupérer le premier et le dernier chiffre
+// 5 - transformer chaque chiffre en integer
+// 6 - sur le premier faire * 10 puis ajouter le dernier chiffre
+// 6 - additionner tous les nombres
