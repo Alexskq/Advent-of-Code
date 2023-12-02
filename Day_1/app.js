831efbbd76859814f8184516af6b86563ebaac45
Zoonekynd Alexandre <alexzoonekynd@gmail.com>
2023-12-02 17:38:27 +0100

    day one finish

--- Day_1/app.js
+++ Day_1/app.js
@@ -1,6 +1,3 @@
-// ? 1 - mettre le text de day1.txt dans une variable de type string
-// ? 2 - split après chaque espace : \n
-// ? 3 - push chaque élement dans un tableau
 import fs from "fs";
 
 // 4 - sur chaque élément itérer sur chaque caractère et récupérer le premier et le dernier chiffre
@@ -18,7 +15,7 @@ function partOne(file) {
   const result = values.reduce((sum, value) => sum + value);
   return result;
 }
-console.log(partOne("./day1.txt"));
+// console.log(partOne("./day1.txt"));
 
 const wordRegExp = new RegExp(
   ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"].join(
@@ -27,10 +24,10 @@ const wordRegExp = new RegExp(
 );
 console.log(wordRegExp);
 
-// convertir les match de regex en nombre
-
 function partTwo() {
   let lines = fs.readFileSync("./example.txt", "utf-8").split("\n");
+
+  let arrFusion = [];
 
   function convertWordToNumber(word) {
     const wordToNumberMap = {
@@ -48,14 +45,73 @@ function partTwo() {
   }
 
   const values = lines.map((line) => {
-    let first = line.split("").find((v) => !Number.isNaN(Number(v)));
-    let firstMatchRegExp = line.match(wordRegExp);
-    let number = firstMatchRegExp.join("");
-    let conversion = convertWordToNumber(number);
+    let firstNumber = line.split("").find((v) => !Number.isNaN(Number(v)));
+    let firstNumberIndex = line
+      .split("")
+      .findIndex((v) => !Number.isNaN(Number(v))); // retourne l'index du premier chiffre trouvé
 
-    console.log(conversion);
+    let firstMatchRegExp = line.match(wordRegExp);
+    let wordIntoNumber = firstMatchRegExp
+      ? convertWordToNumber(firstMatchRegExp.join(""))
+      : null;
+
+    // Reste du code...
+
+    let firstD =
+      firstMatchRegExp &&
+      (firstMatchRegExp.index < firstNumberIndex || firstNumberIndex === -1)
+        ? wordIntoNumber
+        : firstNumber;
+
+    // ! Last number
+
+    const lastWordRegExp = new RegExp(
+      ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+        .join("|")
+        .split("")
+        .reverse()
+        .join("")
+    );
+
+    let lastMatchRegExp = line
+      .split("")
+      .reverse()
+      .join("")
+      .match(lastWordRegExp);
+
+    let lastWord = lastMatchRegExp
+      ? lastMatchRegExp.join("").split("").reverse().join("")
+      : undefined;
+
+    let lastWordIndex = lastMatchRegExp ? lastMatchRegExp.index : -1;
+
+    let lastWordIntoNumber = convertWordToNumber(lastWord);
+    // console.log(lastWordIntoNumber);
+
+    let lastNumber = line
+      .split("")
+      .reverse()
+      .find((v) => !Number.isNaN(Number(v)));
+    let lastNumberIndex = line
+      .split("")
+      .reverse()
+      .findIndex((v) => !Number.isNaN(Number(v)));
+
+    let lastD =
+      (lastMatchRegExp && lastMatchRegExp.index < lastNumberIndex) ||
+      lastNumberIndex == -1
+        ? lastWordIntoNumber
+        : lastNumber;
+
+    let fusion = firstD.toString() + lastD.toString();
+
+    arrFusion.push(Number(fusion));
+
+    return arrFusion;
   });
-  return values;
+  console.log(arrFusion);
+  const result = arrFusion.reduce((sum, value) => sum + value);
+  return result;
 }
 
-partTwo();
+console.log(partTwo());
